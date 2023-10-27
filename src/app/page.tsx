17fce81871e81e9d56c66bc4a44c8df50abd2062
Matías Lorenzo <mlorenzo@xmartlabs.com>
2023-10-27 17:26:42 -0300

    adds a basic layout to all pages that allows configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Home from "./(navFooter)/home/page";
+import Home from "./home/page";
 
 export default function HomePage() {
   return (
---
+++ src/common/base-layout/base-layout.tsx
@@ -0,0 +1,28 @@
+import { Navbar } from '../navbar';
+import { Footer } from '../footer';
+import styles from './base-layout.module.scss';
+
+interface BaseLayoutProps {
+  withNavbar?: boolean;
+  withFooter?: boolean;
+  children: React.ReactNode;
+}
+
+export const BaseLayout : React.FC<BaseLayoutProps> = ({withNavbar = false, withFooter = false, children}) => {
+  if (withNavbar) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.internalContainer}>
+          {withNavbar && <Navbar />}
+          {children}
+        </div>
+        {withFooter && <Footer />}
+      </div>
+    )
+  }
+  return (
+    <>
+      {children}
+    </>
+  )
+}
