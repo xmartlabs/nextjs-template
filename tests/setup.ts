b156cb36434706339d6f6f0476c90afb6532aad2
Gabriel Machin <gabriel.machin@xmartlabs.com>
2024-03-21 16:50:18 -0300

    chore: Fix woodpecker ci (#15)
    * chore: Fix woodpecker ci
    
    * Run prettier write

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,10 @@
-import { loadEnvConfig } from '@next/env'
+import { loadEnvConfig } from "@next/env";
 
 // NOTE: this is needed since Vite loads environment variables on
 // `import.meta.env` while Next loads them on `process.env`.
 const setup = async () => {
-  const projectDir = process.cwd()
-  loadEnvConfig(projectDir)
-}
+  const projectDir = process.cwd();
+  loadEnvConfig(projectDir);
+};
 
 export default setup();
