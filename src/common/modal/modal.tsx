23d743b46296ad8e99844ab1c59385475d2a1719
Maite <martinezf.maite@gmail.com>
2024-11-12 14:42:09 -0300

    Disable scroll when modal is opened

--- src/common/modal/modal.tsx
+++ src/common/modal/modal.tsx
@@ -42,6 +42,15 @@ export const Modal = ({
     };
   }, [onClose]);
 
+  useEffect(() => {
+    if (isOpen) document.body.style.overflow = "hidden";
+    else document.body.style.overflow = "auto";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   let portalRoot = document.getElementById("modal");
